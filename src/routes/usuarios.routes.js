edb803a973f31a6e3e506619e7d0831c6225ee86
Alejandra Barros <alejandrabarros.tds1.senai@gmail.com>
2024-10-31 15:54:18 -0300

    feat: create post user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,17 +2,26 @@ import { Router } from "express";
 import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
-const userslist = new UsersRepository();
-
-
+const usersList = new UsersRepository();
 
 // Rota para listar todos os usuários
 usuariosRoutes.get("/", (req, res) => {
   const usuarios = userslist.getAllUsers();
-  
-    return res.status(200).json({
-        message: usuarios.length == 0 ? "Não há usuários cadastrados!" : `Total de usuários: ${usuarios.length}`,
-    });
+
+  return res.status(200).json({
+    message: usuarios.length == 0 ? "Não há usuários cadastrados!" : `Total de usuários: ${usuarios.length}`,
   });
+});
+
+// Rota para criar um novo usuário
+usuariosRoutes.post("/", (req, res) => {
+  const { nome, email, password } = req.body
+
+  const user = usersList.addUser(nome, email, password);
+  return res.status(201).json({
+    message: "Usuário criado com sucesso!",
+    user,
+  })
+})
 
 export default usuariosRoutes;
