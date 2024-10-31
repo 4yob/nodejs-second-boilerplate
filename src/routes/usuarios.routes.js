614deac7201747192fbeea80299f18e67be51518
Alejandra Barros <alejandrabarros.tds1.senai@gmail.com>
2024-10-31 15:23:41 -0300

    feat: create get all users method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
+import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
+const userslist = new UsersRepository();
 
-let usuarios = [
 
-]
 
 // Rota para listar todos os usuários
 usuariosRoutes.get("/", (req, res) => {
+  const usuarios = userslist.getAllUsers();
+  
     return res.status(200).json({
         message: usuarios.length == 0 ? "Não há usuários cadastrados!" : `Total de usuários: ${usuarios.length}`,
     });
